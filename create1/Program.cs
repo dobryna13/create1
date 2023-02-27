4d957bfe156c31a60c1398e69d98c723adfab67f
dobryna13 <mdobrinchuk@gmail.com>
2023-02-27 22:06:10 +0200

    Добавьте файлы проекта.

---
+++ create1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Список рядків
+        List<string> strings = new List<string> { "Hello", "World", "from", "Maksym" };
+
+        // Створення делегата за допомогою лямбда-виразу
+        Func<string, int> stringLength = s => s.Length;
+
+        // Використання делегата для обробки списку рядків
+        List<int> stringLengths = strings.Select(stringLength).ToList();
+
+        // Виведення результатів
+        foreach (int length in stringLengths)
+        {
+            Console.WriteLine(length);
+        }
+    }
+}
+
